bc9a993fa25bb75c2791d99c7de66d03b7dabfee
danthamGowthami <76723802+danthamGowthami@users.noreply.github.com>
2021-07-21 15:39:34 +0530

    Add files via upload

---
+++ Proscript.js
@@ -0,0 +1,36 @@
+funtion Air()
+{
+document,getElementById("demo0").innerHTML="<h2>Air Pollution</h2>
+<p>From smog hanging over cities to smoke inside the home, air pollution poses a major threat to health and climate.  Ambient air pollution accounts for an estimated 4.2 million deaths per year due to stroke, heart disease, lung cancer, acute and chronic respiratory diseases. 
+
+Around 91% of the world’s population lives in places where air quality levels exceed WHO limits. While ambient air pollution affects developed and developing countries alike, low- and middle-income countries experience the highest-burden, with the greatest toll in the WHO Western Pacific and South-East Asia regions. </p>
+<p>Sources of air pollution are multiple and context-specific. The major outdoor pollution sources include residential energy for cooking and heating, vehicles, power generation,   agriculture/waste incineration, and industry. Policies and investments supporting integrated policies that support sustainable land use, cleaner household energy and transport, energy-efficient housing, power generation, industry, and better municipal waste management can effectively reduce key sources of ambient air pollution. </p>
+<p>Air quality is closely linked to the earth’s climate and ecosystems globally. Many of the drivers of air pollution (i.e. combustion of fossil fuels) are also sources of greenhouse gas emissions. Policies to reduce air pollution, therefore, offer a “win-win” strategy for both climate and health, lowering the burden of disease attributable to air pollution, as well as contributing to the near- and long-term mitigation of climate change.</p>"
+}
+function News()
+{
+document.getElementById("demo1").innerHTML="<h2>Coronavirus live updates: SC issues notice to Centre, UP over 'Kanwar Yatra'</h2>
+<ul>
+<li>A bench headed by Justice R F Nariman issued a notice to the state government and posted the matter for hearing on Friday.</li>
+<li>The Supreme Court on Wednesday took cognisance of the Uttar Pradesh government's decision to allow 'Kanwar Yatra' in the state in the wake of the Covid-19 pandemic.</li>
+<li>Inferences drawn from comparison of HMIS, CRS data on Covid deaths fallacious: Govt</li>
+<li>Covid-19 is under control for now. We're monitoring the situation. Preparations are being done for 3rd wave. Schools will begin for classes 11 and 12 from July 25 at 50% capacity: CM Shivraj Singh Chouhan</li>";
+}
+
+function precautions()
+{
+document.getElementById("demo").innerHTML="<strong>hello</strong>";
+
+document.getElementById("demo").innerHTML="Protect yourself and others around you by knowing the facts and taking appropriate precautions. Follow advice provided by your local health authority.<ul><b><p style="color:red">To prevent the spread of COVID-19:</p><b>
+<li>Clean your hands often. Use soap and water, or an alcohol-based hand rub.</li>
+<li>Maintain a safe distance from anyone who is coughing or sneezing.</li>
+<li>Wear a mask when physical distancing is not possible.
+Don’t touch your eyes, nose or mouth.</li>
+<li>Cover your nose and mouth with your bent elbow or a tissue when you cough or sneeze.
+Stay home if you feel unwell.</li>
+<li>If you have a fever, cough and difficulty breathing, seek medical attention</li>
+</ul>
+<p>Calling in advance allows your healthcare provider to quickly direct you to the right health facility. This protects you, and prevents the spread of viruses and other infections.
+Masks
+Masks can help prevent the spread of the virus from the person wearing the mask to others. Masks alone do not protect against COVID-19, and should be combined with physical distancing and hand hygiene. Follow the advice provided by your local health authority.</p>";
+}
